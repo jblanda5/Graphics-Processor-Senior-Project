91b6c4dd37bd5b7c2944a2c3cdd217d5e3c6469a
jblanda5 <78463169+jblanda5@users.noreply.github.com>
2021-08-18 14:16:42 -0400

    Added project files

---
+++ Project Files/Senior-Project/Senior-Project.srcs/sources_1/new/VGA_Controller.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/18/2021 02:14:53 PM
+// Design Name: 
+// Module Name: VGA_Controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module VGA_Controller(
+    output Vsync,
+    output Hsync,
+    output [3:0] R,
+    output [3:0] G,
+    output [2:0] B
+    );
+endmodule
