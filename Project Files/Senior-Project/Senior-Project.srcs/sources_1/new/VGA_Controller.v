f1d8564d5cf7bf89db3d022878eea7f7367b259a
jblanda5 <78463169+jblanda5@users.noreply.github.com>
2021-09-28 10:40:20 -0400

    Added xpr file and VGA Controller file

--- Project Files/Senior-Project/Senior-Project.srcs/sources_1/new/VGA_Controller.v
+++ Project Files/Senior-Project/Senior-Project.srcs/sources_1/new/VGA_Controller.v
@@ -1,15 +1,15 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: The College of New Jersey
-// Engineer: Jeffrey Blanda
+// Company: 
+// Engineer: 
 // 
-// Create Date: 08/18/2021 02:14:53 PM
-// Design Name: VGA Controller
-// Module Name: VGA_Controller
-// Project Name: Video FX FPGA
-// Target Devices: Nexus 4 DDR
+// Create Date: 09/18/2021 10:27:38 AM
+// Design Name: 
+// Module Name: horizontal_counter
+// Project Name: 
+// Target Devices: 
 // Tool Versions: 
-// Description: This file will generate a VGA signal
+// Description: 
 // 
 // Dependencies: 
 // 
@@ -20,11 +20,60 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module VGA_Controller(
-    output Vsync,
-    output Hsync,
-    output [3:0] R,
-    output [3:0] G,
-    output [2:0] B
+module vga_controller(
+    input clk,
+    output reg Hsync, //Horizontal Sync signal  
+    output reg Vsync,  
+    output reg [7:0] pixel
     );
+    
+wire [10:0]h_count;
+wire [10:0]v_count;
+wire v_clk; //This will be used to increment the vertical counter. Will be high on reset and low elsewhere
+h_counter Hcounter(clk, v_clk, h_count);
+v_counter Vcounter(v_clk, v_count);
+
+always @(posedge clk) begin
+//Vsync
+    if (v_count <= 11'b01001011000) begin //From 1 to 600, Vsync is 0 (low-active) and video is active
+        Vsync <= 0;
+        pixel <= 8'b00000000; //Later this will be removed.
+    end
+    //Front Porch
+    else if (v_count > 11'b01001011000 & v_count <= 11'b01001011001) begin //For 601, Vsync is 0 and video is not active
+        Vsync <= 0;
+        pixel <= 8'b00000000;
+    end
+    //Vsync Pulse
+    else if (v_count > 11'b01001011001 & v_count <= 11'b01001011100) begin //From 602 to 604 Vsync is 1 and video is not active
+        Vsync <= 1;
+        pixel <= 8'b00000000;
+    end
+    //Back Porch
+    else if (v_count > 11'b01001011100 & v_count <= 11'b01001110100) begin //From 605 to 628 Vsync is 0 and video is not active
+        Vsync <= 0;
+        pixel <= 8'b00000000;
+    end
+
+//Hsync
+    if (h_count <= 11'b01100100000) begin //From 1 to 800, Hsync is 0 (low-active) and video is active
+        Hsync <= 0;
+        pixel <= 8'b00000000;
+    end
+    //Front Porch
+    else if (h_count > 11'b01100100000 & h_count <= 11'b01101001000) begin //From 801 to 840, Hsync is 0 and video is not active
+        Hsync <= 0;
+        pixel <= 8'b00000000;
+    end
+    //Hsync Pulse
+    else if (h_count > 11'b01101001000 & h_count <= 11'b01111001000) begin //From 841 to 968 Hsync is 1 and video is not active
+        Hsync <= 1;
+        pixel <= 8'b00000000;
+    end
+    //Back Porch
+    else if (h_count > 11'b01111001000 & h_count <= 11'b10000100000) begin //From 968 to 1056 Hsync is 0 and video is not active
+        Hsync <= 0;
+        pixel <= 8'b00000000;
+    end
+end
 endmodule
