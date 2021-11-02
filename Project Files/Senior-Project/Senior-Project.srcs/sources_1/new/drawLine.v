62ce3c81f1398d211741d0f7f22ea48e48779675
jblanda5 <78463169+jblanda5@users.noreply.github.com>
2021-11-02 18:54:43 -0400

    Made a couple of changes, need to change this file to a State Machine

--- Project Files/Senior-Project/Senior-Project.srcs/sources_1/new/drawLine.v
+++ Project Files/Senior-Project/Senior-Project.srcs/sources_1/new/drawLine.v
@@ -31,31 +31,23 @@ output wire [9:0]x_out,
 output wire [9:0]y_out
 );
 
-//function [9:0]pixelCheck(input[10:0]x, input[10:0]y, input[10:0]deltaX, input [10:0]deltaY); begin
-//    pixelCheck = (deltaY*x)-(deltaX*y);
-//    if (pixelCheck[9] == 1) begin
-//    pixelCheck = ~pixelCheck + 1; //Return the 2's compliment as the value is negative.
-//    end
-//end
-//endfunction
-
 reg [9:0]dx;
 reg [9:0]dy;
-reg [9:0]D; //%%%%Capital D for notation
-//reg [9:0] y; %%%% We don't need this
+reg [9:0]D;
 reg [9:0]y_current;
 reg [9:0]x_current;
 reg increment;
 // Assign outputs to current value + offset
 assign x_out = x_current + x1;
 assign y_out = y_current + y1;
+assign Dnext = D + dy + dy;
 
 initial begin
 ready <= 1;
 end
 
 // Set up initial values given a new line to draw.
-always @(posedge x1, y1, x2, y2) begin
+always @(*) begin
 dx <= x2 - x1;
 // Check for underflow
 if (y2 - y1 > y2) begin
@@ -73,12 +65,12 @@ x_current <= 0;
 ready <= 0;
 end
 
-// %%%% Moved the ~ready check inside the clock condition as I believe this is more efficient? If not we can change it back
-always @(posedge (clk & ~ready)) begin //If the module is not in standbye mode...
-    //This assignment cannot be parallelized otherwise the later reduction this clock cycle will never take place.
-    D = D + dy + dy;
-    
-    if (D > (dx + dx)) begin
+always @(posedge (clk)) begin
+//Add setup state
+
+if (~ready) begin //If the module is not in standbye mode...
+
+    if (Dnext > (dx + dx)) begin
 		
 		// Increment if dy was positive, decrement if negative
 		if (increment) begin
@@ -89,18 +81,18 @@ always @(posedge (clk & ~ready)) begin //If the module is not in standbye mode..
 		end
 		
 		// Reduce D by 2*dx
-        D <= D - (dx + dx);
+        D <= Dnext - (dx + dx);
+    end
+    else begin
+        D <= Dnext;
     end
     
     x_current = x_current + 1; //increment x value
     
-//    x_out = x_current; %%%%Moving these to outside the module, we want to assign xout and yout as their current plus the offset (dx/dy)
-//    y_out = y_current;
-    
-    if (x_current == dx) begin // %%%% Changing this from x2 to dx as we're normalizing the line to points (0,0) and (dx,dy) then offsetting.
+    if (x_current == dx) begin
         ready <= 1;
     end
-
+end
 end
 
 endmodule
